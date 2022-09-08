5db6e2802df135b2a609f64673d5ddb30c001e56
valentina <camilantina@gmail.com>
2022-09-08 00:52:08 -0500

    creacion de spring boot

---
+++ src/test/java/com/CodeMasters/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeMasters.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
