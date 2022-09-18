f0818fc7e11c4705bd1f36fe29d234b5a6d4d857
abraxax777 <jannyalexander@gmail.com>
2022-09-18 17:22:32 -0500

    creacion repositorio empresa empresa controlador y empresa service

---
+++ src/main/java/com/CodeMasters/proyecto/repositorio/EmpresaRepository.java
@@ -0,0 +1,9 @@
+package com.CodeMasters.proyecto.repositorio;
+
+import com.CodeMasters.proyecto.entidades.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+}
