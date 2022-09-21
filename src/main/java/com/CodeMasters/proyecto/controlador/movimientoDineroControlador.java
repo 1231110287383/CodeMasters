06d6e29a8dadc7fc6176bf1dc04184d7e7aa4544
valentina <camilantina@gmail.com>
2022-09-20 20:56:31 -0500

    clases de MovimientoDinero2

--- src/main/java/com/CodeMasters/proyecto/controlador/movimientoDineroControlador.java
+++ src/main/java/com/CodeMasters/proyecto/controlador/movimientoDineroControlador.java
@@ -1,4 +1,42 @@
 package com.CodeMasters.proyecto.controlador;
 
+import com.CodeMasters.proyecto.entidades.MovimientoDinero;
+import com.CodeMasters.proyecto.servicios.movimientoDineroServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class movimientoDineroControlador {
+
+    @Autowired
+    movimientoDineroServicios movimientoDineroServicio;
+
+    @GetMapping("/enterprises/movements")
+    public List<MovimientoDinero> vermovimientoDinero() {
+        return movimientoDineroServicio.getAllMovimientoDinero();
+    }
+
+    @PostMapping("/enterprises/[id]/movements/post")
+    public MovimientoDinero guardarMovimiento(@RequestBody MovimientoDinero movimientoDinero){
+        return movimientoDineroServicio.saveOrUpdateMovimientoDinero(movimientoDinero);
+    }
+
+    @GetMapping("/enterprises/[id]/movements/get")
+    public MovimientoDinero movimientoDineroPorId(@PathVariable("id") long id){
+        return movimientoDineroServicio.getMovimentoDineroById(id);
+    }
+
+    @PatchMapping("/enterprises/[id]/movements/patch")
+    public MovimientoDinero actualizarMovimientoDinero(@PathVariable("id")long id, @RequestBody MovimientoDinero x){
+        MovimientoDinero m = movimientoDineroServicio.getMovimentoDineroById(id);
+        m.setConceptoMovimiento(x.getConceptoMovimiento());
+        m.setMontoDinero(x.getMontoDinero());
+        m.setUsuarioMovimiento(x.getUsuarioMovimiento());
+        return movimientoDineroServicio.saveOrUpdateMovimientoDinero(m);
+    }
+
+
+
 }
